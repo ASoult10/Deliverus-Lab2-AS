507f0f453d507977b73bcd98ea45262214cb22e7
Alejandro Soult <asoulttoscano@gmail.com>
2024-03-01 16:40:46 +0100

    Rutas

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,13 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
+  app.route('/restaurants').get(RestaurantController.index)
+  app.route('/restaurants/:restaurantId').get(RestaurantController.show)
+  
+  app.route('/restaurants').post(RestaurantController.create)
+  //app.route('/restaurants/:restaurantId').get(RestaurantController.show) No se puede hacer porque la función no está implementada en el controller
+  app.route('/restaurants/:restaurantId').put(RestaurantController.update)
+  app.route('/restaurants/:restaurantId').delete(RestaurantController.destroy)
 
 }
 export default loadFileRoutes
